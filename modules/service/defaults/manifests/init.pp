894ec4860f88805358a09c6e2268b6a3e71bbf02
Scot P. floess <sfloess@nc.rr.com>
2010-11-27 10:13:54 -0500

    Used a more condensed notation for the maps of static hosts

--- modules/service/defaults/manifests/init.pp
+++ modules/service/defaults/manifests/init.pp
@@ -17,68 +17,50 @@ class defaults {
 
     // ----------------------------------------------------
 
-    $host_gateway = {
-        subIp => "1",
-        name  => "gateway",
-    }
-
-    $host_amy = {
-        subIp => "2",
-        name  => "amy",
-    }
-
-    $host_hale = {
-        subIp => "3"
-        name  => "hale",
-    }
-
-    $host_playstation = {
-        subIp => "4",
-        name  => "playstation",
-    }
-
-    $host_itouch = {
-        subIp => "5",
-        name  => "itouch",
-    }
-
-    $host_psp = {
-        subIp => "6",
-        name  => "psp",
-    }
-
-    $host_flossy = {
-        subIp => "7",
-        name  => "flossy",
-    }
-
-    $host_adminserver = {
-        subIp => "253",
-        name  => "adminserver",
-        cname => [
-            "fileserver",
-            "fontserver",
-            "mailserver",
-            "puppet",
-            "webserver",
-            "www"
-        ]
-    }
-
-    $host_accesspoint = {
-        subIp => "254",
-        name  => "accesspoint",
-    }
-
     $dns_defaultHosts = [
-        $host_gateway,
-        $host_amy,
-        $host_hale,
-        $host_playstation,
-        $host_itouch,
-        $host_psp,
-        $host_flossy,
-        $host_adminserver,
-        $host_accesspoint,
+        {
+            subIp => "1",
+            name  => "gateway",
+        },
+        {
+            subIp => "2",
+            name  => "amy",
+        },
+        {
+            subIp => "3"
+            name  => "hale",
+        },
+        {
+            subIp => "4",
+            name  => "playstation",
+        },
+        {
+            subIp => "5",
+            name  => "itouch",
+        },
+        {
+            subIp => "6",
+            name  => "psp",
+        },
+        {
+            subIp => "7",
+            name  => "flossy",
+        },
+        {
+            subIp => "253",
+            name  => "adminserver",
+            cname => [
+                "fileserver",
+                "fontserver",
+                "mailserver",
+                "puppet",
+                "webserver",
+                "www"
+            ]
+        },
+        {
+            subIp => "254",
+            name  => "accesspoint",
+        },
     ]
 }
